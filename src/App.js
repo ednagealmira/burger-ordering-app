bf9dd798f8c17d97fbc5e3962f68377a2af1c09a
Ednagea Almira <ednagea.almira@gmail.com>
2021-08-15 13:25:00 +0700

    desktop frontend update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,36 @@ import Count from "./Count.js";
 function App() {
   return (
     <div className="container">
-      <Count order="Burger Deluxe" price="7.39" />
-      <Count order="Supreme Cheeseburger" price="8.19" />
-      <Count order="Turkey Burger" price="6.49" />
-      <Count order="Spicy Chicken Sandwich" price="7.19 " />
-      <Count order="Menu A" price="7.19 " />
-      <Count order="Menu B" price="7.19 " />
+      <Count
+        order="Burger Deluxe"
+        price="7.39"
+        imglink="https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"
+      />
+      <Count
+        order="Supreme Cheeseburger"
+        price="8.19"
+        imglink="https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=364&q=80"
+      />
+      <Count
+        order="Turkey Burger"
+        price="6.49"
+        imglink="https://images.unsplash.com/photo-1566217688581-b2191944c2f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
+      />
+      <Count
+        order="Spicy Chicken Sandwich"
+        price="7.19 "
+        imglink="https://images.unsplash.com/photo-1606755962773-d324e0a13086?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+      />
+      <Count
+        order="Onion Rings & Fries"
+        price="6.29"
+        imglink="https://images.pexels.com/photos/6941010/pexels-photo-6941010.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      />
+      <Count
+        order="Chicken Strips"
+        price="7.59 "
+        imglink="https://images.unsplash.com/photo-1548340748-6d2b7d7da280?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+      />
     </div>
   );
 }
--- src/Count.js
+++ src/Count.js
@@ -2,23 +2,46 @@ import { useState } from "react";
 import "./App.css";
 
 const Count = (props) => {
+  // const button = document.querySelectorAll("button");
+  // if (count <= 0) {
+  //   button.disabled = true;
+  // } else {
+  //   button.disabled = false;
+  // }
   const [count, setCount] = useState(0);
+  const [button, setButton] = useState(true);
   const add = () => {
+    if (count == 0) {
+      setButton(false);
+    }
     setCount(count + 1);
   };
   const minus = () => {
     if (count > 0) {
       setCount(count - 1);
     }
+    if (count == 1) {
+      setButton(true);
+    }
   };
+
   return (
     <div className="menu">
-      <h3>{props.order}</h3>
-      <p>${props.price}</p>
-      <div className="counter">
-        <button onClick={add}>+</button>
-        <p>{count}</p>
-        <button onClick={minus}>-</button>
+      <div className="container-img">
+        <img src={props.imglink}></img>
+      </div>
+      <div className="container-content">
+        <h3>{props.order}</h3>
+        <p>${props.price}</p>
+        <div className="counter">
+          <button disabled={button} onClick={minus} className="btn-left">
+            -
+          </button>
+          <p>{count}</p>
+          <button onClick={add} className="btn-right">
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
